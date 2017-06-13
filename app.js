10f931d65fe47e24c2eeef1ed3515052a0db205d
KajG <kaaiman99@hotmail.com>
2017-06-13 14:53:55 +0200

    im gay

--- app.js
+++ app.js
@@ -1,6 +1,87 @@
-console.log("Hello world!!!@@!?L@e'wd;e'f");
-var a = 0;
-for (var i = 0; i <= 10000; i++) {
-	a++;
-	console.log("ig  to a bone to p ick" + a);
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(3000, "0.0.0.0");
+console.log("Server has started");
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		name:"Loser",
+		pressingRight:false,
+		pressingLeft:false,
+		pressingUp:false,
+		pressingDown:false,
+		maxSpd:10
+	}
+	self.updatePosition = function(){
+		if(self.pressingRight)
+			self.x+= self.maxSpd;
+		if(self.pressingLeft)
+			self.x-= self.maxSpd;
+		if(self.pressingUp)
+			self.y-= self.maxSpd;
+		if(self.pressingDown)
+			self.y+= self.maxSpd;
+	}
+	return self;
 }
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+	socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+	});
+
+	socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+    });
+
+    socket.on('nameChange', function(data){
+    	player.name = data.name;
+    });
+});
+
+
+setInterval(function() {
+	var pack = [];
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
+		pack.push({
+			x:player.x,
+			y:player.y,
+			name:player.name
+		});
+	}
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPos', pack);
+	}
+},1000/25);
